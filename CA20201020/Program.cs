c4b621fca1ace6df92e57dcfa66a968eaac79c0a
Roland Zajkó <zajkorolandvlszki@outlook.hu>
2020-10-20 17:49:34 +0200

    Add project files.

---
+++ CA20201020/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA20201020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] t = new int[10];
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                Console.WriteLine(r.Next(10, 100));
+            }
+            Console.ReadKey();
+        }
+    }
+}
